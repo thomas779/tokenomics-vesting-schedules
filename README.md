3ead8d706c556e2573347cbfc6258bec3a06d3a7
thomas779 <thomasehumphreys@gmail.com>
2022-08-07 11:31:31 +0200

    convert txt to csv

--- README.md
+++ README.md
@@ -3,9 +3,8 @@ Something cleaner can be done here, but for a fixed emissions curve this should 
 run `python3 vesting_csv.py`
 
 Notes:
-- file with emissions in .txt (change manually to .csv)
 - lists with NULL values mean cliff month active
 
 To-do:
-- convert txt to csv
-- chart the emissions schedule
+- [x] convert txt to cs
+- [ ] chart the emissions schedule
--- vesting_csv.py
+++ vesting_csv.py
@@ -1,3 +1,5 @@
+import csv
+
 class emissionType: # constructor
     def __init__(self, token_quantity, start_month, lock_period, unlock_period):
         self.token_quantity = token_quantity
@@ -28,9 +30,9 @@ f = open("emission.txt", "w+")
 # create header
 header = []
 for c in emissionSources:
-    header.append(namestr(c,globals())[0])
+    header.append(namestr(c, globals())[0])
 header.append('total')
-f.write(str(header)[1:-1]+'\n')
+f.write(str(header)[1:-1] + '\n')
 
 start_monthTotal = 60
 
@@ -39,15 +41,22 @@ for i in range(start_monthTotal):
     cumEmission = []
     DV = 0
     for j in emissionSources:
-        if(j.start_month+j.lock_period>i):
+        if (j.start_month + j.lock_period > i):
             cumEmission.append(0)
-        elif(j.start_month+j.lock_period+j.unlock_period>=i):
-            cumEmission.append((j.token_quantity/j.unlock_period)*(i-(j.start_month+j.lock_period)))
+        elif (j.start_month + j.lock_period + j.unlock_period >= i):
+            cumEmission.append((j.token_quantity / j.unlock_period) * (i - (j.start_month + j.lock_period)))
         else:
             cumEmission.append(j.token_quantity)
         DV += cumEmission[-1]
     cumEmission.append(DV)
 
-    f.write(str(cumEmission)[1:-1]+'\n')
+    f.write(str(cumEmission)[1:-1] + '\n')
 
 f.close()
+
+with open('emission.txt', 'r') as in_file:
+    stripped = (line.strip() for line in in_file)
+    lines = (line.split(",") for line in stripped if line)
+    with open('emission.csv', 'w') as out_file:
+        writer = csv.writer(out_file)
+        writer.writerows(lines)
